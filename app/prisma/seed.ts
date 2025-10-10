eeae7a2d518748cde5f3d55ee0e071d53a219bea
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-10 09:41:08 +0300

    feat: Добавлен наполнитель Stages, в общий сидер

--- app/prisma/seed.ts
+++ app/prisma/seed.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import { seedDistricts } from "./seed/01_districts";
 import { seedCategories } from "./seed/02_categories";
+import { seedStages } from "./seed/03_stages";
 
 const prisma = new PrismaClient();
 
 async function main() {
   await seedDistricts();
   await seedCategories();
+  await seedStages();
 }
 
 main()
