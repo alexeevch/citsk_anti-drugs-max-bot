771bc6df1f9655ef47a48bedc6b962bc7c693fab
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-12 00:27:28 +0300

    fix: Правка типов bot-started.controller.ts

--- app/src/bot/controllers/bot-started.controller.ts
+++ app/src/bot/controllers/bot-started.controller.ts
@@ -1,12 +1,12 @@
-import type { ExtendedContext } from "~/shared/types/bot.types.js";
+import type { ExtendedContext, SessionData } from "../bot.types.js";
+import { getBotStartedMessage } from "../utils/template.util.js";
 import type { CreateUserPayload } from "~/shared/types/entity.types.js";
 import { userRepository } from "~/core/repositories/user.repository.js";
-import { getBotStartedMessage } from "~/bot/utils/template.util.js";
 
 export const botStartedController = async (ctx: ExtendedContext) => {
   if (ctx.user.is_bot) return;
 
-  ctx.sessionData = {};
+  ctx.sessionData = {} as SessionData;
 
   const me: CreateUserPayload = {
     userId: ctx.user?.user_id,
