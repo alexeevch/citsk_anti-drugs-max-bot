8f2f4df3676ad144d2461f463cd23a9548f03b79
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-11 23:05:09 +0300

    feat: Обновлены типы бота

--- app/src/shared/types/bot.types.ts
+++ app/src/shared/types/bot.types.ts
@@ -1,6 +1,12 @@
 import { Context } from "@maxhub/max-bot-api";
+import type { User } from "@maxhub/max-bot-api/types";
 
 export interface ExtendedContext extends Context {
+  sessionData: SessionData;
+  user: UserBot;
+}
+
+export interface SessionData {
   currentStage?: Stage;
   photoToken?: string;
   complaintMessage?: string;
@@ -8,13 +14,20 @@ export interface ExtendedContext extends Context {
     latitude: number;
     longitude: number;
   };
+  categoryId?: number;
+  districtId?: number;
 }
 
-export type Command = "/start" | "/help" | "rules";
+//Расширил, т.к. на момент разработки типы библиотеки неактуальны
+export type UserBot = User & {
+  first_name: string;
+};
 
 export type Stage =
+  | "start"
   | "category_choose"
   | "district_choose"
   | "complaint_description"
   | "photo_send"
-  | "location_send";
+  | "location_send"
+  | "finish";
