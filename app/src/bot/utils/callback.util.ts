c746ed93cdf6722a2bc605cbef454e4a1fc13496
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-13 01:42:42 +0300

    feat: Утилиты для работы с callback keyboard данными

--- app/src/bot/utils/callback.util.ts
+++ app/src/bot/utils/callback.util.ts
@@ -1,3 +1,18 @@
-export const splitCallback = (payload: string) => {
-  return payload.split(":")[1];
+import type { CallbackPayloadData } from "~/bot/bot.types.js";
+import type { Stage } from "~/bot/utils/enum.util.js";
+
+export const convertStringToPayload = (string: string): CallbackPayloadData | null => {
+  const data = string.split(":");
+
+  if (data.length !== 3) return null;
+
+  return {
+    stage: data[0] as Stage,
+    name: String(data[1]),
+    id: Number(data[2]),
+  };
+};
+
+export const convertPayloadToString = (data: CallbackPayloadData): string => {
+  return `${data.stage}:${data.name}:${data.id}`;
 };
