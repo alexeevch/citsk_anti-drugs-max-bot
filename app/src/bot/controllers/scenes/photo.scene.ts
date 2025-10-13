5c5e0399b2060790d863ca068f0c2d0a52bb9716
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-13 03:08:02 +0300

    feat: Черновая сцена обработки фотографий

---
+++ app/src/bot/controllers/scenes/photo.scene.ts
@@ -0,0 +1,61 @@
+import type { ExtendedContext } from "~/bot/bot.types.js";
+import { Stage } from "~/bot/utils/enum.util.js";
+import type { SceneContract } from "~/bot/contracts/scene.contract.js";
+import { locationSendMessage } from "~/bot/utils/template.util.js";
+import { convertStringToPayload } from "~/bot/utils/callback.util.js";
+import { COMPLAINT_LIMITS } from "~/bot/bot.config.js";
+import { Keyboard } from "@maxhub/max-bot-api";
+import { buildInlineKeyboard } from "~/bot/utils/keyboard.util.js";
+
+export const photoScene: SceneContract = {
+  async handle(ctx: ExtendedContext) {
+    if (ctx.currentStage !== Stage.PhotoSend) return;
+
+    const callbackRaw = ctx.callback?.payload;
+
+    if (callbackRaw) {
+      const payloadData = convertStringToPayload(callbackRaw);
+      if (!payloadData) {
+        await ctx.reply("Не получилось пропустить отправку фото. Попробуйте позже.");
+      }
+      if (payloadData?.stage !== Stage.PhotoSend) return;
+      if (payloadData?.id === 0) {
+        await ctx.reply(`✅ Вы пропустили отправку фото`);
+      }
+    }
+
+    if (ctx.message && ctx.message.body.attachments) {
+      const photos = ctx.message.body.attachments.filter((item) => item.type === "image");
+      const photosCount = photos.length;
+      if (photosCount > COMPLAINT_LIMITS.MAX_PHOTO_COUNT) {
+        await ctx.reply(
+          `❌ Вы превысили количество фотографий. Загрузите **до ${COMPLAINT_LIMITS.MAX_PHOTO_COUNT}** штук.`,
+          { format: "markdown" }
+        );
+        return;
+      }
+
+      if (photosCount > 0) {
+        ctx.complaint.photos = photos.map(({ payload }) => {
+          return {
+            token: payload.token,
+            url: payload.url,
+          };
+        });
+
+        await ctx.reply(`✅ Вы успешно загрузили **${photosCount}** фото!`, {
+          format: "markdown",
+        });
+      }
+    }
+
+    const requestLocationBtn = Keyboard.button.requestGeoLocation("Отметить на карте");
+    await ctx.reply(locationSendMessage, {
+      attachments: [buildInlineKeyboard([requestLocationBtn])],
+      format: "markdown",
+    });
+
+    ctx.currentStage = Stage.LocationSend;
+    console.log(ctx.complaint);
+  },
+};
