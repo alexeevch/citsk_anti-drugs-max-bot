dc117213585a0af1eab9a595681d4abbb9ffaccb
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-13 16:57:45 +0300

    feat: Обновление регулярных выражений

--- app/src/shared/utils/regex.util.ts
+++ app/src/shared/utils/regex.util.ts
@@ -1,9 +1,11 @@
 import { COMPLAINT_LIMITS } from "~/bot/bot.config.js";
 
 export const MINIMAL_MESSAGE_LENGTH_REGEXP = new RegExp(
-  `^.{${COMPLAINT_LIMITS?.MIN_MESSAGE_LENGTH ?? 10},}$`
+  `^.{${COMPLAINT_LIMITS?.MIN_MESSAGE_LENGTH ?? 10},}$`,
+  "s"
 );
 export const MINIMAL_ADDRESS_LENGTH_REGEXP = new RegExp(
-  `^.{${COMPLAINT_LIMITS?.MIN_ADDRESS_LENGTH ?? 10},}$`
+  `^.{${COMPLAINT_LIMITS?.MIN_ADDRESS_LENGTH ?? 5},}$`,
+  "s"
 );
 export const COMMAND_REGEXP = /^\/[a-zA-Z]+$/;
