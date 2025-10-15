67c214c8f1101c5aa6253a646d325e40e1a502fc
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-15 09:56:20 +0300

    fix: Удален http express

--- app/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-export default function buildApp() {
-  const app = express();
-
-  app.use(express.json());
-
-  return app;
-}
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,27 +1,9 @@
 import "dotenv/config";
-import buildApp from "~/app.js";
-import morgan from "morgan";
 import { MaxBotApp } from "~/bot/bot.app.js";
 
-const port = process.env.APP_PORT || 3000;
-
 const main = async () => {
-  const app = buildApp();
-
-  const environment = process.env.NODE_ENV || "development";
-  app.use(environment === "development" ? morgan("dev") : morgan("tiny"));
-
-  const server = app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-
   const botApp = await MaxBotApp.create(process.env.BOT_TOKEN!);
   await botApp.start();
-
-  process.on("SIGTERM", async () => {
-    console.log("SIGTERM signal received: closing services");
-    server.close(() => console.log("HTTP server closed"));
-  });
 };
 
 main().catch((error) => {
